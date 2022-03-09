11954aa7a9b92023fba8c47aba7c455a7c462eac
xueming-wang <wxueming0614@hotmail.com>
2022-03-09 18:15:32 +0100

    affiche wordpress

--- srcs/requirements/mariadb/conf/db_init.sh
+++ srcs/requirements/mariadb/conf/db_init.sh
@@ -1,10 +1,24 @@
 service mysql start
+sleep 3
 
-sleep 5
+mysql -e "create database ${DB_NAME};"
+sleep 3
 
-mysql -u root -e "create database ${MARIADB_DATABASE};"
-mysql -u root -e "create user ${MARIADB_USER}@'%' identified by '${MARIADB_PASSWORD}';"
-mysql -u root -e "grant all privileges on ${MARIADB_DATABASE}.* to '${MARIADB_USER}'@'%'; flush privileges;"
-mysql -u root -e "alter user ${MARIADB_USER}@'%' identified by '${MARIADB_PASSWORD}'; flush privileges;"
-service mysql restart
+mysql -e "create user ${DB_USER}@'%' identified by '${DB_USER_PWD}';"
+sleep 3
+
+mysql -e "grant all privileges on ${DB_NAME}.* to '${DB_USER}'@'%'; flush privileges;"
+sleep 3
+
+mysql -e "alter user  'root'@'localhost' identified by '${DB_ROOT_PSW}'; 
+flush privileges;"
+
+
+# echo "create database ${MARIADB_DATABASE};" | mysql -u root
+# echo "create user  '${MARIADB_USER}'@'%' identified by ${MARIADB_PASSWORD};" | mysql -u root
+# echo "grant all privileges on ${MARIADB_DATABASE}.* to '${MARIADB_USER}'@'%' with grant option;" | mysql -u root
+# echo "flush privileges" | mysql -u root
+
+
+
 
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'xuwang' );
 define( 'DB_PASSWORD', 'mypassword' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadb:3306' );
+define( 'DB_HOST', 'mysql:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
