6f6bc95e0d79e9d1c4c8754910ea4919a21c7712
xueming-wang <wxueming0614@hotmail.com>
2022-03-17 19:41:20 +0100

    2user is ok

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-service mysql restart
+service mysql start
 
-echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" | mysql
-echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';"  | mysql
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
+
+echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME};" | mysql -u root 
+echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PWD}';"  | mysql -u root
+echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%'" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_HOST_PSW}';"| mysql -u root
+# echo "FLUSH PRIVILEGES;" | mysql -u root
+
 
 mysql -u root "wordpress" < /APP/wp.sql
 
